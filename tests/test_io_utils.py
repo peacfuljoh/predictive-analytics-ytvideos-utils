468838f692820f2930f7448e0c77d6cb948b5be8
Johannes <you@example.com>
2023-12-13 17:49:31 -0500

    added pickle I/O and test

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -1,7 +1,7 @@
 
 import tempfile
 
-from src.ytpa_utils.io_utils import save_json, load_json
+from src.ytpa_utils.io_utils import save_json, load_json, save_pickle, load_pickle
 
 
 
@@ -9,7 +9,14 @@ from src.ytpa_utils.io_utils import save_json, load_json
 def test_save_load_json():
     obj = {'a': {'b': [1, 2, 3], 'c': [5, 4, 'a']}, 'd': [1, 2, 3], 'e': '2432'}
 
-    with tempfile.NamedTemporaryFile() as filename:
-        save_json(filename.name, obj)
-        assert load_json(filename.name) == obj
+    with tempfile.NamedTemporaryFile() as file:
+        save_json(file.name, obj)
+        assert load_json(file.name) == obj
+
+def test_save_load_pickle():
+    obj = {'a': {'b': [1, 2, 3], 'c': [5, 4, 'a']}, 'd': [1, 2, 3], 'e': '2432'}
+
+    with tempfile.NamedTemporaryFile() as file:
+        save_pickle(file.name, obj)
+        assert load_pickle(file.name) == obj
 
